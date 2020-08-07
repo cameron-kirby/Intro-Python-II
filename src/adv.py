33e2d33f372d8c9193e74039c9fee3b94e673ac4
Cameron Kirby <cameronevankirby@gmail.com>
2020-08-07 11:23:12 -0500

    Added MVP Functionality

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,54 @@
 from room import Room
+from player import Player
+from item import Item
+
+## TEST Covers
+# Comprehensions
+# Classes / Inheritance
+# Using a module
+# Look for test files
+# To pass the sprint challenge, the test files must pass
+# Make sure retro is in before lunch
+
+# Declare all items
+item = {
+    'sword': Item(
+        "Sword", 
+        "A shabby looking weapon"
+    )
+}
 
 # Declare all the rooms
-
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside': Room(
+        "Outside Cave Entrance", 
+        "North of you, the cave mount beckons",
+        []
+    ),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer': Room(
+        "Foyer",
+        """Dim light filters in from the south. Dusty passages run north and east.""",
+        [item['sword']]
+    ),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room(
+        "Grand Overlook", 
+        """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.""",
+        []
+    ),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow': Room(
+        "Narrow Passage", 
+        """The narrow passage bends here from west to north. The smell of gold permeates the air.""",
+        []
+    ),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room(
+        "Treasure Chamber", 
+        """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.""",
+        []
+    ),
 }
 
 
@@ -38,6 +68,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player(room['outside'],[])
 
 # Write a loop that:
 #
@@ -49,3 +80,76 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    #
+    # * Prints the current room name
+    print(f"\n{player.location}\n")
+    if len(player.location.items) > 0:
+        print("Items in room:")
+        for element in player.location.items:
+            print(element)
+        print("\n")
+        
+    # * Prints the current description (the textwrap module might be useful here).
+    # * Waits for user input and decides what to do.
+    #
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    #
+    # If the user enters "q", quit the game.
+    command = input("What would you like to do?\n> ").split(' ')
+    ## ADD INSTRUCTIONS 
+    ## 
+    if len(command) > 1: # Action commands
+        if command[0] == 'take' or command[0] == 'grab': # Take/Grab item
+            # Go through all items in room, check if that item exists, if it does, add it to the bag, if it does not exist, notify
+            for item in player.location.items:
+                if item.name.lower() == command[1].lower():
+                    player.addItem(item)
+                    player.location.removeItem(item)
+
+        if command[0] == 'drop':
+            for item in player.inventory:
+                if item.name.lower() == command[1].lower():
+                    player.dropItem(item)
+                    player.location.addItem(item)
+
+        if command[0] == 'check' and command[1] == 'bag': # Check bag
+            player.checkBag()
+
+    else: # Movement commands
+        if command[0] == 'q': # Quit
+            break
+        elif command[0] == 'n': # Move North
+            # check if the player can move to the north
+            # if there is, set that north room as the player's location
+            #### FIGURE OUT FSTRING THING
+            try:
+                if player.location.n_to:
+                    player.setLocation(player.location.n_to)
+            except:
+                print("There is no room to your north")
+        elif command[0] == 's': # Move South
+            try:
+                if player.location.s_to:
+                    player.setLocation(player.location.s_to)
+            except:
+                print("There is no room to your south")
+        elif command[0] == 'e': # Move East
+            try:
+                if player.location.e_to:
+                    player.setLocation(player.location.e_to)
+            except:
+                print("There is no room to your east")
+        elif command[0] == 'w': # Move West
+            try:
+                if player.location.w_to:
+                    player.setLocation(player.location.w_to)
+            except:
+                print("There is no room to your west")
+        elif command[0] == 'i' or command[0] == 'inventory':
+            player.checkBag()
+
+        
+        
--- src/player.py
+++ src/player.py
@@ -1,2 +1,26 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, location, inventory):
+        self.location = location
+        self.inventory = inventory
+
+    def setLocation(self, location):
+        self.location = location
+
+    def addItem(self, item):
+        self.inventory.append(item)
+
+    def dropItem(self, item):
+        index = self.inventory.index(item)
+        self.inventory.pop(index)
+
+    def checkBag(self):
+        if len(self.inventory) > 0:
+            print("Your bag contains:")
+            for item in self.inventory:
+                print(item.name)
+        else:
+            print("Your bag is empty")
+
